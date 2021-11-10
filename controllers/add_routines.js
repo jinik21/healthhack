7019afa63efee7bc7a9081e3d368e51243aa1253
jinik21 <nikheeljain@outlook.com>
2021-11-10 23:16:41 +0530

    fixrd riutines

--- controllers/add_routines.js
+++ controllers/add_routines.js
@@ -2,18 +2,21 @@ const newRoutine = async (req, resp, User) => {
 
     const usr = await User.findOne({ 'email': req.body.email });
     try {
-        var data=usr.routines;
         var rs={
             date:req.body.date,
             link:req.body.link
         }
-        data.push(rs);
+        // data.push(rs);
 
-        usr.update({routines:data},function (err, result) {
+        usr.update({ 
+            $addToSet: {
+                routines: rs
+            }
+        },function (err, result) {
             if (err){
                 console.log(err)
             }else{
-                console.log("Result :", result) 
+                // console.log("Result :", result) 
             }})
             usr.save()
         resp.json("Routine Sucessfully added");
